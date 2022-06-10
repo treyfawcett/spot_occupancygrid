8a67d4f4d346d0a007c11f16250254372ccca088
humanoid2050 <humanoid2050@monolith>
2022-06-09 21:33:59 -0400

    work in progress

--- occupancy_grid_service.py
+++ occupancy_grid_service.py
@@ -18,24 +18,44 @@ from bosdyn.client.directory_registration import (DirectoryRegistrationClient,
 
 from bosdyn.client.fault import FaultClient
 from bosdyn.client.server_util import GrpcServiceRunner, populate_response_header
+from bosdyn.client.world_object import WorldObjectClient, make_add_world_object_req
 
 from bosdyn.client.util import setup_logging
 
 from bosdyn.client.robot_state import RobotStateClient
 from bosdyn.client.async_tasks import AsyncPeriodicQuery, AsyncTasks
 
-from bosdyn.client.math_helpers import Quat, SE3Pose
-from bosdyn.client.frame_helpers import get_odom_tform_body
-
 from bosdyn.api import local_grid_pb2, local_grid_service_pb2_grpc 
 
+from bosdyn.client.frame_helpers import *
+from bosdyn.client.math_helpers import *
+from bosdyn.api import geometry_pb2 as geom
+from bosdyn.api import world_object_pb2
+
+from skimage.draw import line
 
 
 DIRECTORY_NAME = 'occupancy-grid'
 AUTHORITY = 'occupancy-grid'
 SERVICE_TYPE = 'bosdyn.api.LocalGridService'
 
+GRID_FRAME_NAME='occupancy-grid'
+
 LOGGER = logging.getLogger(__name__)
+
+
+def get_point_cloud_data_in_grid_frame(pcl):
+    """
+    Create a 3 x N numpy array of points in the grid frame. 
+    :return: a 3 x N numpy array in the seed frame.
+    """
+    
+    cloud = pcl
+    #TODO: vision, odom, or ground plane?
+    vision_tform_cloud = get_a_tform_b(cloud.source.transforms_snapshot, VISION_FRAME_NAME,
+                                     cloud.source.frame_name_sensor)
+    point_cloud_data = np.frombuffer(cloud.data, dtype=np.float32).reshape(int(cloud.num_points), 3)
+    return vision_tform_cloud.transform_cloud(point_cloud_data)
 
 
 
@@ -47,8 +67,6 @@ def _update_thread(async_task):
 
 
 class AsyncPointCloud(AsyncPeriodicQuery):
-    """Grab robot state."""
-
     def __init__(self, point_cloud_client, pcl_sources):
         super(AsyncPointCloud, self).__init__("point_clouds", point_cloud_client, LOGGER,
                                               period_sec=0.2)
@@ -57,6 +75,13 @@ class AsyncPointCloud(AsyncPeriodicQuery):
 
     def _start_query(self):
         return self._client.get_point_cloud_from_sources_async(self._sources)
+
+class AsyncFiducialObjects(AsyncPeriodicQuery):
+    def __init__(self, query_name, client, period_sec):
+        super().__init__(query_name, client, LOGGER, period_sec)
+
+    def _start_query(self):
+        return self._client.list_world_objects_async(object_type=[world_object_pb2.WORLD_OBJECT_APRILTAG])
 
 class PointCloudProxy:
     def __init__(self, robot, pcl_service: str, pcl_sources: List[str], use_async: bool = True) -> None:
@@ -90,30 +115,83 @@ class PointCloudProxy:
     def get_latest(self):
         pcl = self._point_cloud_task.proto[0].point_cloud
         if pcl:
-            data = np.fromstring(pcl.data, dtype=np.float32)
+            data = get_point_cloud_data_in_grid_frame(pcl)
             t = pcl.source.acquisition_time
 
 
+def gen_grid_frametree(grid_anchor_x, grid_anchor_y, grid_anchor_angle, anchor_april_tag, apriltag_frametree):
+    #we expect the fiducial to be posted on a wall, and its normal (z axis) defining the 
+    # x direction of the grid
 
-   
+    anchortag_tform_grid = SE3Pose(position=Vec3(grid_anchor_x,grid_anchor_y,0),rotation=Quat.from_yaw(grid_anchor_angle*math.pi/180))
 
+    frame_tree_edges = {}
+    frame_tree_edges = add_edge_to_tree(frame_tree_edges, anchortag_tform_grid, f'filtered_fiducial_{anchor_april_tag:03}', GRID_FRAME_NAME)
+
+    for edge in apriltag_frametree.child_to_parent_edge_map:
+        frame_tree_edges = add_edge_to_tree(frame_tree_edges, edge.parent_tform_child*anchortag_tform_grid, edge.parent_frame_name, GRID_FRAME_NAME)
+
+    # Pack the dictionary into a FrameTreeSnapshot proto message.
+    return geom.FrameTreeSnapshot(child_to_parent_edge_map=frame_tree_edges)
+
+
+#constantly update the apriltag frame-tree in memory
+#all we really care about though is the x/y/theta
+def find_anchor_fiducial_transform_snapshot(apriltag_world_objects, anchor_april_tag):
+    for apriltag in apriltag_world_objects:
+        if apriltag.apriltag_properties.tag_id != anchor_april_tag:
+            continue
+        if apriltag.apriltag_properties.fiducial_filtered_pose_status != world_object_pb2.AprilTagProperties.AprilTagPoseStatus.STATUS_OK:
+            raise RuntimeError('fiducial location not accurately know')
+        # Look for the custom frame that was included in the add-request, where the child frame name was "my_special_frame"
+        return apriltag.transforms_snapshot
+    raise KeyError('fiducial not found')
+        
+            
 class OccupancyGrid:
     def __init__(self, grid_width, grid_height, grid_scale, anchor_april_tag, 
                     grid_anchor_x, grid_anchor_y, grid_anchor_angle, 
-                    data_floor, data_ceiling, 
+                    data_floor, data_ceiling, max_ray_trace,
                     logger=None) -> None:
         #compute static grid transform to tag
-        pass
+        self.grid_width = grid_width
+        self.grid_height = grid_width
+        self.grid_scale = grid_scale
+        
+        #observation grid (unknown, ray traced free, occupied))
+        self._data = np.zeros((grid_width, grid_height), dtype=np.uint8)
+        #occupancy belief(floating point)
 
-    def add_pcl(self, pcl):
-        # transform to tag grid frame via tag frame
-        pass
+
+    def add_pcl(self, anchor, pcl):
+        # transform to grid frame via anchor tag frame
+        start = self._real_to_grid(anchor.x, anchor.y)
+        for point in pcl:
+            end = self._real_to_grid(point.x, point.y)
+
+
+            pixels = line(*start, *end) #([x coords], [y coords])
+            
+            np.subtract.at(self._data, (pixels[0][:-1],pixels[1][:-1]), 10)
+
+            self._data[pixels[0][-1],pixels[1][-1]] += 20
+        
 
     def get_current_grid(self):
         pass
 
     def clear(self):
         pass
+
+    def _real_to_grid(self, x, y):
+        return x/self.grid_scale, y/self.grid_scale
+
+    def _is_in_grid(self, x, y):
+        return x >= 0 and x < self.grid_width and y >=0 and y < self.grid_height
+
+    def _clip_to_grid(self, start, end):
+        #could enter and leave grid....
+        return start, end
 
 
 class OccupancyGridServicer(local_grid_service_pb2_grpc.LocalGridServiceServicer):
@@ -174,14 +252,24 @@ class OccupancyGridServicer(local_grid_service_pb2_grpc.LocalGridServiceServicer
 
     def GetLocalGridTypes(self, request, context):
         response = local_grid_pb2.GetLocalGridTypesResponse()
-        for grid in self.occupancy_grids.values():
-            response.local_grid_type.add().CopyFrom(grid.image_source_proto)
+        for grid in self.grids.values():
+            response.local_grid_type.add().CopyFrom(grid.localgrid_type)
         populate_response_header(response, request)
         return response
 
     def GetLocalGrids(self, request, context):
-        pass
-
+        response = local_grid_pb2.GetLocalGridsResponse()
+        for grid_request in request.local_grid_requests:
+            grid_resp = response.local_grid_responses.add()
+            src_name = grid_request.local_grid_type_name
+            grid_resp.local_grid_type_name.CopyFrom(src_name)
+            if src_name not in self.grids.keys():
+                grid_resp.status = local_grid_pb2.LocalGridResponse.STATUS_NO_SUCH_GRID
+                self.logger.warning("Occupancy grid '%s' is unknown.", src_name)
+                continue
+            
+            grid_resp.local_grid.CopyFrom(self.grids[src_name].grid_proto)
+        populate_response_header(response, request)
 
 
 def make_occupancy_grid_service(bosdyn_sdk_robot, service_name, pcl_source_names,
@@ -275,15 +363,6 @@ def main(argv):
     with keep_alive:
         service_runner.run_until_interrupt()
 
-
-    
-    _task_list = [_point_cloud_task]
-    _async_tasks = AsyncTasks(_task_list)
-    print('Connected.')
-
-    update_thread = threading.Thread(target=_update_thread, args=[_async_tasks])
-    update_thread.daemon = True
-    update_thread.start()
 
 
 if __name__=='__main__':
